4058134bc48149d0a8598c6921003fd419047735
Rahul Madhavan <rahul.maddy@gmail.com>
2018-02-03 18:18:44 +0530

    taking cd1 hidden state from reconstruction as probability vector

--- Assignment4-RBMs/cd1.m
+++ Assignment4-RBMs/cd1.m
@@ -5,13 +5,24 @@ function ret = cd1(rbm_w, visible_data)
     %error('not yet implemented');
     %disp(size(rbm_w));
     %disp(size(visible_data));
+    %hidden_state = sample_bernoulli(visible_state_to_hidden_probabilities(rbm_w, visible_data));
+    %disp(size(hidden_state));
+    %visible_state2= sample_bernoulli(hidden_state_to_visible_probabilities(rbm_w, hidden_state));
+    %disp(size(visible_state2));
+    %hidden_state2 = sample_bernoulli(visible_state_to_hidden_probabilities(rbm_w,visible_state2));
+    %disp(size(hidden_state2));
+    %Goodness_Grad_initial = configuration_goodness_gradient(visible_state2, hidden_state2);
+    %Goodness_Grad_final = configuration_goodness_gradient(visible_data, hidden_state);
+    %ret = Goodness_Grad_final - Goodness_Grad_initial; 
+    %changing the final hidden state to non sampled
     hidden_state = sample_bernoulli(visible_state_to_hidden_probabilities(rbm_w, visible_data));
     %disp(size(hidden_state));
     visible_state2= sample_bernoulli(hidden_state_to_visible_probabilities(rbm_w, hidden_state));
     %disp(size(visible_state2));
-    hidden_state2 = sample_bernoulli(visible_state_to_hidden_probabilities(rbm_w,visible_state2));
+    hidden_state2 = visible_state_to_hidden_probabilities(rbm_w,visible_state2);
     %disp(size(hidden_state2));
     Goodness_Grad_initial = configuration_goodness_gradient(visible_state2, hidden_state2);
     Goodness_Grad_final = configuration_goodness_gradient(visible_data, hidden_state);
     ret = Goodness_Grad_final - Goodness_Grad_initial; 
+    
 end
